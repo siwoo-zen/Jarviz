c40b2838cbe2e48f246026ef996c14ae6ed03003
siwoo-zen <seazonablerain@gmail.com>
2025-05-05 04:15:49 +0900

    기업사용자 채용공고생성 및 모델 밑바탕 생성

--- workhub/models.py
+++ workhub/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.conf import settings
+
 
 User = get_user_model()
 
@@ -31,3 +33,25 @@ class Resume(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.user.username})"
+    
+
+class JobPost(models.Model):
+    employer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={'is_company': True},
+        related_name='workhub_jobposts'
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    location = models.CharField(max_length=100)
+    salary = models.CharField(max_length=100)
+    deadline = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.employer.username}"
+
+    class Meta:
+        ordering = ['-created_at']
--- workhub/urls.py
+++ workhub/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import ResumeCreateView, ResumeUpdateView, ResumeListView, ResumeDetailView
+from .views import JobPostCreateView, JobPostUpdateView, JobPostListView
 from workhub.views import main_dashboard
 
 app_name = 'workhub'
@@ -10,6 +11,7 @@ urlpatterns = [
     path('resume/<int:pk>/edit/', ResumeUpdateView.as_view(), name='resume_edit'),
     path('resume/list/', ResumeListView.as_view(), name='resume_list'),
     path('resume/<int:pk>/', ResumeDetailView.as_view(), name='resume_detail'),
-    #path('jobs/create/', JobCreateView.as_view(), name='job_create'),
-
+    path('jobs/create/', JobPostCreateView.as_view(), name='job_create'),
+    path('jobs/<int:pk>/edit/', JobPostUpdateView.as_view(), name='job_edit'),
+    path('jobs/list/', JobPostListView.as_view(), name='job_list'),
 ]
